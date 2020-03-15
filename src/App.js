f28dd0a0eadc7a48b40b2d1e81965150f4e66e95
Alejandro <acuellarh@gmail.com>
2020-03-15 15:30:00 -0500

    setState and onClick to button add

--- src/App.js
+++ src/App.js
@@ -2,14 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count : 0      
+    };
+
+    // Enlazar el método al contexto actual
+    this.updateValue = this.updateValue.bind(this);
+  }
+ 
+  updateValue() {
+    this.setState({
+      count : this.state.count + 1
+    });
+  }
+
   render() {
     return (
-      <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+      <div>        
+        <span className="value">{this.state.count}</span>
+        <button id="inc" onClick={this.updateValue}>Incrementa</button>
       </div>
     );
   }
+
+
 }
 
 export default App;
